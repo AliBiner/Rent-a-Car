3d25b7f9465ecaac50f0c15f9b3887ddbaef5a7c
Ali Biner <alibiner.github@gmail.com>
2025-09-14 18:22:40 +0300

    Change welcome text

--- presentation/src/main/java/App.java
+++ presentation/src/main/java/App.java
@@ -2,7 +2,7 @@ import UI.HomeUI;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("=======WELCOME TO RENT A CAR APP==========");
+        System.out.println("=======Araç Kiralama Uygulamasına Hoşgeldiniz==========");
 
         HomeUI homeUI = new HomeUI();
         homeUI.view();
