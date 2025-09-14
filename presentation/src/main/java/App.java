739e75cd281fe89553c3b06b1a386234339b2101
Ali Biner <alibiner.github@gmail.com>
2025-09-14 13:52:40 +0300

    Welcome Message and route home ui

---
+++ presentation/src/main/java/App.java
@@ -0,0 +1,10 @@
+import UI.HomeUI;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("=======WELCOME TO RENT A CAR APP==========");
+
+        HomeUI homeUI = new HomeUI();
+        homeUI.view();
+    }
+}
