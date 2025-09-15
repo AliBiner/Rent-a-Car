da30c6f4685e1fe0193c189e7c675796560170b3
Ali Biner <alibiner.github@gmail.com>
2025-09-16 02:07:35 +0300

    add isAlreadyExist method and refactoring

--- service/src/main/java/services/UserService.java
+++ service/src/main/java/services/UserService.java
@@ -1,6 +1,9 @@
 package services;
 
 import dto.request.UserSignInRequestDto;
+import dto.response.IResponseDto;
+import dto.response.ResponseDto;
+import dto.response.UserSignInResponseDto;
 import entity.User;
 import mappers.DtoToUser;
 import repository.UserRepository;
@@ -14,18 +17,54 @@ public class UserService {
 
     UserRepository userRepository = new UserRepository();
 
-    public int save(UserSignInRequestDto dto) throws SQLException, NoSuchAlgorithmException {
+    public ResponseDto save(UserSignInRequestDto dto) throws SQLException, NoSuchAlgorithmException {
+
+        ResponseDto responseDto = new ResponseDto();
+
 
         if (isAlreadyExist(dto.getEmail())) {
-            return -1; //Todo response dto oluşturulacak
+            responseDto.setStatusCode(400);
+            responseDto.setMessage("Bu email adresi zaten kayıtlı!");
+            responseDto.setSuccess(false);
+            return responseDto;
         }
 
+        //Hash password
         dto.setPassword(PasswordHashing.hash(dto.getPassword()));
 
+        //Dto Mapping
         User user =  DtoToUser.userSignInRequestDtoTo(dto);
         user.setCreatedDate(LocalDateTime.now());
 
-        return userRepository.save(user);
+        //Insert DB
+        int resultSave = userRepository.save(user);
+
+        //Kayıt işlemi başarısız durumu
+        if (resultSave==0){
+            responseDto.setStatusCode(400);
+            responseDto.setMessage("Veri tabanına veri kaydedilemedi!");
+            responseDto.setSuccess(false);
+            return responseDto;
+        }
+
+        //Kayıt işlemi başarılı durumu
+        User result = userRepository.getByEmail(dto.getEmail());
+
+        responseDto.setStatusCode(200);
+        responseDto.setMessage("Kayıt İşlemi Başarılı");
+        responseDto.setSuccess(true);
+
+        //Response Body
+        UserSignInResponseDto signInResponseDto = new UserSignInResponseDto();
+
+
+        signInResponseDto.setId(result.getId());
+        signInResponseDto.setEmail(result.getEmail());
+        signInResponseDto.setFullName(result.getFullName());
+
+        responseDto.setBody(signInResponseDto);
+
+        return responseDto;
     }
 
     public boolean isAlreadyExist(String email) throws SQLException {
