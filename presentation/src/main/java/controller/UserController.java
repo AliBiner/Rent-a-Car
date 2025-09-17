ece6697b987d93300c3f190ec441f5b5dda6fcb0
Ali Biner <alibiner.github@gmail.com>
2025-09-17 13:32:41 +0300

    config try catch for exception handler

--- presentation/src/main/java/controller/UserController.java
+++ presentation/src/main/java/controller/UserController.java
@@ -3,14 +3,18 @@ package controller;
 import dto.request.UserSignInRequestDto;
 
 import dto.response.ResponseDto;
+import exceptionHandler.ExceptionHandler;
 import services.UserService;
 import validations.UserValidation;
+
+import java.sql.SQLException;
 
 public class UserController {
 
     public ResponseDto signIn(UserSignInRequestDto dto){
         ResponseDto responseDto = new ResponseDto();
         try {
+
             final String isValidResult = UserValidation.UserSignInRequestDtoValidation.isValid(dto);
             if (!isValidResult.equals("")){
                 responseDto.setStatusCode(400);
@@ -18,15 +22,17 @@ public class UserController {
                 responseDto.setSuccess(false);
                 return responseDto;
             }
+
+
             //Kayıt işlemleri başladı
             UserService userService = new UserService();
             return userService.save(dto);
-        }catch (Exception e){
-            responseDto.setStatusCode(500);
-            responseDto.setMessage(e.getMessage());
-            responseDto.setSuccess(false);
-            return responseDto;
+        }
+        catch (Exception e){
+            return new ExceptionHandler(e,"").handle();
         }
 
+
     }
+
 }
