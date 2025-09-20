7b94903af3d9419e125f7d9c6bc6efb773bcfe34
Ali Biner <alibiner.github@gmail.com>
2025-09-20 03:58:39 +0300

    edit by custom exceptions

--- presentation/src/main/java/com/alibiner/controller/UserController.java
+++ presentation/src/main/java/com/alibiner/controller/UserController.java
@@ -2,69 +2,47 @@ package com.alibiner.controller;
 
 import com.alibiner.dto.request.UserLoginRequestDto;
 import com.alibiner.dto.request.UserSignInRequestDto;
-
 import com.alibiner.dto.response.ResponseDto;
 import com.alibiner.dto.response.UserLoginResponseDto;
-import com.alibiner.entity.User;
+import com.alibiner.dto.response.UserSignInResponseDto;
+import com.alibiner.errorMessages.ErrorCode;
 import com.alibiner.exceptionHandler.ExceptionHandler;
+import com.alibiner.exceptions.ValidationException;
 import com.alibiner.services.UserService;
 import com.alibiner.validations.UserValidation;
-
-import java.security.NoSuchAlgorithmException;
-import java.sql.SQLException;
 import java.util.*;
 
 public class UserController {
 
     public ResponseDto signIn(UserSignInRequestDto dto){
-        ResponseDto responseDto = new ResponseDto();
         try {
-            final String isValidResult = UserValidation.UserSignInRequestDtoValidation.isValid(dto);
-            if (!isValidResult.equals("")){
-                responseDto.setStatusCode(400);
-                responseDto.setMessage(isValidResult);
-                responseDto.setSuccess(false);
-                return responseDto;
+            final List<String> errorMessages = UserValidation.UserSignInRequestDtoValidation.validate(dto);
+            if (!errorMessages.isEmpty()){
+                throw new ValidationException(ErrorCode.VALIDATION,errorMessages);
             }
-
 
             //Kayıt işlemleri başladı
             UserService userService = new UserService();
-            return userService.save(dto);
+            UserSignInResponseDto responseDto = userService.save(dto);
+            return new ResponseDto(ErrorCode.OK.getCode(), ErrorCode.OK.getMessage(), true, responseDto);
         }
         catch (Exception e){
             return new ExceptionHandler(e,"").handle();
         }
-
-
     }
 
     public ResponseDto login(UserLoginRequestDto dto){
         try {
-            ResponseDto responseDto;
             List<String> errorMessages = UserValidation.UserLoginRequestDtoValidation.validate(dto);
 
             if (!errorMessages.isEmpty()){
-                responseDto = new ResponseDto();
-                responseDto.setStatusCode(400);
-                responseDto.setMessage("Bilgiler geçersiz!");
-                responseDto.setSuccess(false);
-                responseDto.setBody(errorMessages);
-                return responseDto; //Todo - responseDto'yu validate methodunda doldur.
+                throw new ValidationException(ErrorCode.VALIDATION,errorMessages);
             }
 
-            //Todo - service katmanına gönder
-
             UserService userService = new UserService();
-            ResponseDto loginResponseDto = userService.login(dto);
+            UserLoginResponseDto loginResponseDto = userService.login(dto);
 
-
-            responseDto = new ResponseDto();
-            responseDto.setSuccess(true);
-            responseDto.setMessage("Giriş işlemi başarılı!");
-            responseDto.setStatusCode(200);
-            responseDto.setBody(loginResponseDto);
-            return responseDto;
+            return new ResponseDto("200","Giriş işlemi başarılı!",true,loginResponseDto);
         } catch (Exception e) {
             return new ExceptionHandler(e,e.getMessage()).handle();
         }
