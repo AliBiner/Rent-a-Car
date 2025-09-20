fc0c572e7153e32c73242a96d13fc5f235a683d4
Ali Biner <alibiner.github@gmail.com>
2025-09-20 04:01:03 +0300

    update by Encapsulation

--- presentation/src/main/java/com/alibiner/UI/UserSession.java
+++ presentation/src/main/java/com/alibiner/UI/UserSession.java
@@ -1,32 +1,40 @@
 package com.alibiner.UI;
 
 public class UserSession {
-    private static int id;
-    private static String firstName;
-    private static String lastName;
+    private static int id=0;
+    private static String firstName="";
+    private static String lastName="";
 
 
     public static int getId() {
         return id;
     }
 
-    public static void setId(int id) {
-        UserSession.id = id;
-    }
-
     public static String getFirstName() {
         return firstName;
-    }
-
-    public static void setFirstName(String firstName) {
-        UserSession.firstName = firstName;
     }
 
     public static String getLastName() {
         return lastName;
     }
 
-    public static void setLastName(String lastName) {
+
+    public static void setSession(int id, String firstName, String lastName){
+        UserSession.id = id;
+        UserSession.firstName = firstName;
         UserSession.lastName = lastName;
+    }
+
+
+    public static boolean isEmpty(){
+        if (id >0 && !firstName.isBlank() && !lastName.isBlank())
+            return false;
+        return true;
+    }
+
+    public static void clear(){
+        UserSession.id = 0;
+        UserSession.lastName = "";
+        UserSession.firstName = "";
     }
 }
