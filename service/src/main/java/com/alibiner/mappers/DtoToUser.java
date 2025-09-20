1aa98d7dda84d636e5f4240bc80380082c14b7e5
Ali Biner <alibiner.github@gmail.com>
2025-09-20 03:57:02 +0300

    add password hash in mapping

--- service/src/main/java/com/alibiner/mappers/DtoToUser.java
+++ service/src/main/java/com/alibiner/mappers/DtoToUser.java
@@ -2,16 +2,23 @@ package com.alibiner.mappers;
 
 import com.alibiner.dto.request.UserSignInRequestDto;
 import com.alibiner.entity.User;
+import com.alibiner.util.PasswordHashing;
+
+import java.security.NoSuchAlgorithmException;
+import java.time.LocalDateTime;
 
 public class DtoToUser {
 
-    public static User userSignInRequestDtoTo(UserSignInRequestDto dto){
+    public static User userSignInRequestDtoTo(UserSignInRequestDto dto) throws NoSuchAlgorithmException {
         User user = new User();
 
         user.setFirstName(dto.getFirstName());
         user.setLastName(dto.getLastName());
         user.setEmail(dto.getEmail());
-        user.setPassword(dto.getPassword());
+        user.setCreatedDate(LocalDateTime.now());
+
+        //hash
+        user.setPassword(PasswordHashing.hash(dto.getPassword()));
 
         return user;
     }
