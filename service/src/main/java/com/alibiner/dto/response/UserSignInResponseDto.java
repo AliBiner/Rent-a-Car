f8676c6eebe85e8a3e24a6b785f1c1981cc6788f
Ali Biner <alibiner.github@gmail.com>
2025-09-20 04:03:30 +0300

    optimize by Encapsulation and smart constructor

--- service/src/main/java/com/alibiner/dto/response/UserSignInResponseDto.java
+++ service/src/main/java/com/alibiner/dto/response/UserSignInResponseDto.java
@@ -5,28 +5,22 @@ public class UserSignInResponseDto{
     private String email;
     private String fullName;
 
-    public int getId() {
-        return id;
+    public UserSignInResponseDto(int id, String email, String fullName) {
+        this.id = id;
+        this.email = email;
+        this.fullName = fullName;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public int getId() {
+        return id;
     }
 
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public String getFullName() {
         return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
     }
 
     @Override
