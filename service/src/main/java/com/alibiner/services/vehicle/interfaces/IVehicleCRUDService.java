da59b690be9da276b19f69d91050c56efadb3975
Ali Biner <alibiner.github@gmail.com>
2025-09-24 04:31:01 +0300

    initial IVehicleCRUDService

--- service/src/main/java/com/alibiner/services/vehicle/interfaces/IVehicleCRUDService.java
+++ service/src/main/java/com/alibiner/services/vehicle/interfaces/IVehicleCRUDService.java
@@ -3,6 +3,7 @@ package com.alibiner.services.vehicle.interfaces;
 import com.alibiner.dto.request.vehicle.IVehicleRequestDto;
 import com.alibiner.dto.response.vehicle.IVehicleResponseDto;
 import com.alibiner.exceptions.DataNotInsertException;
+import com.alibiner.exceptions.car.NotFoundException;
 import com.alibiner.exceptions.user.UserForbiddenException;
 import com.alibiner.exceptions.user.UserNotFoundException;
 
@@ -11,8 +12,8 @@ import java.util.*;
 
 public interface IVehicleCRUDService {
     boolean save(IVehicleRequestDto dto) throws UserForbiddenException, UserNotFoundException, SQLException, DataNotInsertException;
-    IVehicleResponseDto getById(int id);
-    List<IVehicleResponseDto> getAll();
-    boolean update(IVehicleRequestDto car);
-    boolean delete(int id);
+    IVehicleResponseDto getById(int id) throws SQLException;
+    List<IVehicleResponseDto> getAll(int adminId) throws UserForbiddenException, UserNotFoundException, SQLException;
+    boolean update(IVehicleRequestDto vehicle) throws SQLException, NotFoundException, UserForbiddenException, UserNotFoundException;
+    boolean delete(int id, int userId) throws UserForbiddenException, UserNotFoundException, SQLException;
 }
