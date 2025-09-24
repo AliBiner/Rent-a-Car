d60997831925cd309c2cfc7c508fb3d8dca90472
Ali Biner <alibiner.github@gmail.com>
2025-09-24 04:28:49 +0300

    IVehicleCRUDService implementation

--- service/src/main/java/com/alibiner/services/vehicle/CarService.java
+++ service/src/main/java/com/alibiner/services/vehicle/CarService.java
@@ -2,13 +2,16 @@ package com.alibiner.services.vehicle;
 
 import com.alibiner.dto.request.vehicle.IVehicleRequestDto;
 import com.alibiner.dto.request.vehicle.car.CarAddRequestDTO;
+import com.alibiner.dto.request.vehicle.car.CarUpdateRequestDto;
 import com.alibiner.dto.response.vehicle.IVehicleResponseDto;
-import com.alibiner.dto.response.vehicle.car.CarAddResponseDTO;
+import com.alibiner.dto.response.vehicle.car.AdminGetCarResponseDto;
 import com.alibiner.entity.Car;
 import com.alibiner.enums.errorMessages.ErrorCode;
 import com.alibiner.exceptions.DataNotInsertException;
+import com.alibiner.exceptions.car.NotFoundException;
 import com.alibiner.exceptions.user.UserForbiddenException;
 import com.alibiner.exceptions.user.UserNotFoundException;
+import com.alibiner.mappers.CarToDto;
 import com.alibiner.mappers.DtoToCar;
 import com.alibiner.repository.CarRepository;
 import com.alibiner.services.UserService;
@@ -17,6 +20,8 @@ import com.alibiner.util.MyDbConnection;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CarService implements IVehicleCRUDService {
@@ -24,7 +29,7 @@ public class CarService implements IVehicleCRUDService {
     private UserService userService;
     private CarRepository carRepository;
 
-    public CarService() {
+    public CarService()  {
         this.connection = MyDbConnection.getInstance().getConnection();
         userService = new UserService();
         carRepository = new CarRepository(this.connection);
@@ -48,22 +53,72 @@ public class CarService implements IVehicleCRUDService {
     }
 
     @Override
-    public IVehicleResponseDto getById(int id) {
-        return null;
+    public IVehicleResponseDto getById(int id) throws SQLException {
+        Car car =  carRepository.getById(id);
+        return  CarToDto.toAdminGetCarResponseDto(car);
     }
 
     @Override
-    public List<IVehicleResponseDto> getAll() {
-        return List.of();
+    public List<IVehicleResponseDto> getAll(int adminId) throws UserForbiddenException, UserNotFoundException, SQLException {
+        if (!userService.isAdmin(adminId)) {
+            throw new UserForbiddenException(ErrorCode.FORBIDDEN);
+        }
+
+        List<Car> carList = carRepository.getAll();
+        ArrayList<IVehicleResponseDto> resultList = new ArrayList<>();
+
+        for (Car car : carList){
+            resultList.add(CarToDto.toAdminGetCarResponseDto(car));
+        }
+
+        return resultList;
     }
 
     @Override
-    public boolean update(IVehicleRequestDto car) {
-        return false;
+    public boolean update(IVehicleRequestDto vehicle) throws SQLException, NotFoundException, UserForbiddenException, UserNotFoundException {
+
+        // Yetki kontrolü
+        CarUpdateRequestDto dto = (CarUpdateRequestDto) vehicle;
+        if (!userService.isAdmin(dto.atUpdated())) {
+            throw new UserForbiddenException(ErrorCode.FORBIDDEN);
+        }
+
+        Car car = carRepository.getById(dto.id());
+
+        if (car==null){
+            throw new NotFoundException(ErrorCode.NOT_FOUND);
+        }
+
+        Car newCar = new Car(
+                car.getId(),
+                dto.brand(),
+                dto.model(),
+                car.isRent(),
+                car.getCreatedDate(),
+                LocalDateTime.now(),
+                car.getAtCreatedId(),
+                null,
+                dto.atUpdated(),
+                null,
+                dto.doorCount(),
+                dto.machineType(),
+                dto.productionYear());
+
+
+        if (carRepository.update(newCar)<=0){
+            return false;
+        }
+        connection.commit();
+        return true;
     }
 
     @Override
-    public boolean delete(int id) {
-        return false;
+    public boolean delete(int id, int userId) throws UserForbiddenException, UserNotFoundException, SQLException {
+        if (!userService.isAdmin(userId)) {
+            throw new UserForbiddenException(ErrorCode.FORBIDDEN);
+        }
+        boolean result = carRepository.delete(id);
+        connection.commit();
+        return result;
     }
 }
