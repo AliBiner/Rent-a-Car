582013c69ed9c9168aad322022a6ebd3480815e1
Ali Biner <alibiner.github@gmail.com>
2025-09-24 04:29:14 +0300

    add car to dto mapping

---
+++ service/src/main/java/com/alibiner/mappers/CarToDto.java
@@ -0,0 +1,22 @@
+package com.alibiner.mappers;
+
+import com.alibiner.dto.response.vehicle.car.AdminGetCarResponseDto;
+import com.alibiner.entity.Car;
+
+public class CarToDto {
+    public static AdminGetCarResponseDto toAdminGetCarResponseDto(Car car){
+        return new AdminGetCarResponseDto(
+                car.getId(),
+                car.getBrand(),
+                car.getModel(),
+                car.isRent(),
+                car.getDoorCount(),
+                car.getMachineType(),
+                car.getProductionYear(),
+                car.getCreatedDate(),
+                car.getUpdatedDate(),
+                car.getAtCreatedId(),
+                car.getAtCreatedId()
+        );
+    }
+}
